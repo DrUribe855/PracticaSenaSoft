8aa46435ced319bfe864e53dd1c30cfe9d5eadec
Yeison Martinez <yeisonl2036@gmail.com>
2023-09-19 21:26:38 -0500

    Model relationships added

---
+++ app/Models/Store.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    public $table = "stores";
+    protected fillable = array("*");
+
+    public function orders(){
+        return this->belongsToMany(Order::class, "orders");
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,33 +12,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    public $table = "users";
+    protected fillable = array("*");
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function stores(){
+        return this->belongsToMany(Store::class, "stores");
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
